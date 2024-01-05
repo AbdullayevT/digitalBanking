5f15e062345fd32788604a4f6060ddfb65e8ef9e
Tapdig Abdullayev <abdullayevtapdiq2@gmail.com>
2024-01-05 19:34:54 +0400

    testing vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import * as path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/digitalBanking",
+  base: "/digitalBanking/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
